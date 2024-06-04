93e3b4c191e8983aa9ee7b11a8e0e909ec10a8ee
Adri <aagudo1818@gmail.com>
2024-06-04 20:56:43 +0200

    GREEN: Given home team and away team initializes the new match with proper attribute values

--- src/main/java/LiveScoreBoard.java
+++ src/main/java/LiveScoreBoard.java
@@ -1,13 +1,23 @@
 import model.Match;
+import model.Score;
 import model.Team;
 
+import java.time.LocalDateTime;
 import java.util.HashSet;
 
 public class LiveScoreBoard {
-    private final HashSet<Match> scoreBoard = new HashSet<>();
+    private final HashSet<Match> scoreBoard = new HashSet<>(); //TODO make it thread safe
 
     public void startNewMatch(Team homeTeam, Team awayteam){
-        scoreBoard.add(new Match());
+        var match = new Match();
+        match.setHomeTeam(homeTeam);
+        match.setAwayTeam(awayteam);
+        var score = new Score();
+        score.setHomeTeamScoredGoals(0);
+        score.setAwayTeamScoredGoals(0);
+        match.setScore(score);
+        match.setStartTime(LocalDateTime.now()); //TODO make it timezone safe
+        scoreBoard.add(match);
     }
 
     public HashSet<Match> getScoreBoard() {
